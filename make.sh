e4a0831c2af688672903042f93dcf290c64f4374
Mark Raynsford <code@io7m.com>
2024-05-23 10:56:58 +0000

    Add HTML/css.

--- make.sh
+++ make.sh
@@ -5,9 +5,11 @@ mkdir -p out
 
 cp -v src/*.png out/
 cp -v src/feed.xml out/
+cp -v src/index.html out/
+cp -v src/style.css out/
+
 rsync -av src/corrupt out/
 rsync -av src/rc1N out/
 rsync -av src/rc11 out/
 rsync -av src/rcN1 out/
 rsync -av src/rcNM out/
-
